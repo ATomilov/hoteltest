01b2880ca478c57d2eee57df531728dfc26e31a6
Alexey Tomilov <apofeosz@yandex.ru>
2018-08-23 04:00:04 +0600

    Added first test task

---
+++ 1/main.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+  // $('#t').addClass('a');
+  // $('.c').css('color', 'green');
+});
+document.addEventListener("DOMContentLoaded", function() {
+  var table = document.getElementById('t');
+  table.classList.add('a');
+});
